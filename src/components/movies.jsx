6f472fab98a8bc8b3dc21c3785d8cd54fc3423f9
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-12 14:08:34 +0200

    18- Deleting a Movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,36 +6,48 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
-  handleDelete = (movie) => {};
+  handleDelete = (movie) => {
+    this.setState(
+      this.state.movies.splice(this.state.movies.indexOf(movie), 1)
+    );
+  };
 
   renderTable() {
-    if (this.state.movies.length === 0)
-      return <p>There are no movies in the database</p>;
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database</p>;
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => (
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
             <tr>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <button className="btn btn-danger btn-sm">Delete</button>
-              </td>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 
