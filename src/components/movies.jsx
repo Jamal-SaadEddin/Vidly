ef4ad35780e97d56fb1987c83b4c6f1974941031
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-20 18:25:44 +0200

    Revert "render ListGroup - Jamal Solution"
    This reverts commit 3c1df8fe7b07d70b5e04a15e953b2d4b0f7ae3d3.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
-import ListGroup from "./common/ListGroup";
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
-    genres: getGenres(),
     pageSize: 4,
     currentPage: 1,
   };
@@ -33,18 +30,16 @@ class Movies extends Component {
   };
 
   renderTable() {
-    const { length: countMovies } = this.state.movies;
-    const { length: countGenres } = this.state.genres;
-    const { pageSize, currentPage, movies: allMovies, genres } = this.state;
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
-    if (countMovies === 0) return <p>There are no movies in the database</p>;
+    if (count === 0) return <p>There are no movies in the database</p>;
 
     const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
       <React.Fragment>
-        <ListGroup countGenres={countGenres} genres={genres} />
-        <p>Showing {countMovies} movies in the database.</p>
+        <p>Showing {count} movies in the database.</p>
         <table className="table">
           <thead>
             <tr>
@@ -82,7 +77,7 @@ class Movies extends Component {
           </tbody>
         </table>
         <Pagination
-          itemsCount={countMovies}
+          itemsCount={"kka"} //itemsCount={count}
           pageSize={pageSize}
           currentPage={currentPage}
           onPageChange={this.handlePageChange}
