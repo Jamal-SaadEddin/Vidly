1db66f26d6b6b4ccac19ecbb60c29ee4db97b65b
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-20 21:30:30 +0200

    12- Filtering- Handling Selection

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 const ListGroup = (props) => {
-  const { items, valueProperety, textProperety } = props;
+  const { items, valueProperety, textProperety, selectedItem, onItemSelect } =
+    props;
 
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <li key={item[valueProperety]} className="list-group-item">
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperety]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
           {item[textProperety]}
         </li>
       ))}
@@ -14,11 +21,6 @@ const ListGroup = (props) => {
   );
 };
 
-// The reason of this change: is that assume we have
-// a lot of ListGrouo components at the movies component,
-// so we don't have to repeat passing these props at each component,
-// so to reduce code writing and complexity of passing them manually.
-// we added them here for only one time, and we got the same results.
 ListGroup.defaultProps = {
   valueProperety: "_id",
   textProperety: "name",
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -37,7 +37,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    console.log(genre);
+    this.setState({ selectedGenre: genre });
   };
 
   renderTable() {
@@ -53,6 +53,7 @@ class Movies extends Component {
         <div className="col-3">
           <ListGroup
             items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
