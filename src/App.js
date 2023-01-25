22520edf0dfa04fb9ebfba6cc45a1f72ad589b76
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-25 14:37:47 +0200

    17- Adding the NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from "react";
-import "./App.css";
 import Movies from "./components/movies";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" exact to="/movies" />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/navBar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <Link className="navbar-brand" to="/">
+        Vidly
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
