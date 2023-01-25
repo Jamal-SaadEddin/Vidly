0e2bbb5e11030c4c9a6efffbe5f3379e650268c4
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-25 14:46:51 +0200

    18- Linking to the MovieForm

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import "./App.css";
+import MovieForm from "./components/movieForm";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
