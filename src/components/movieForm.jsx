110e3628414c931e13b0bbeebecb2204d4442b5b
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-25 14:25:59 +0200

    15- Adding React Router

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieForm = () => {
+  return (
+    <div>
+      <h1>Movie Form</h1>
+    </div>
+  );
+};
+
+export default MovieForm;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
