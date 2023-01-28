f386aa9c069bf44002cfec3343a1bbf8be31855e
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-28 14:03:57 +0200

    12- Validation on Change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -30,10 +30,26 @@ class LoginForm extends Component {
     console.log("Submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+      // ...
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+      // ...
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
