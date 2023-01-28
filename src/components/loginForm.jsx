c04371ee2568bae4c96b76fecbb693c47e7ec55f
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-28 15:47:24 +0200

    13- Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -7,7 +8,17 @@ class LoginForm extends Component {
     errors: {},
   };
 
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
@@ -33,11 +44,11 @@ class LoginForm extends Component {
   validateProperty = ({ name, value }) => {
     if (name === "username") {
       if (value.trim() === "") return "Username is required.";
-      // ...
+      // ... other rules
     }
     if (name === "password") {
       if (value.trim() === "") return "Password is required.";
-      // ...
+      // ... other rules
     }
   };
 
