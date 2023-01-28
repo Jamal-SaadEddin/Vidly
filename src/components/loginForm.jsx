ca4181604067ac8a308bfb3d097615f4ca3cdd82
Jamaloooo <jamalsa3d2001@gmail.com>
2023-01-28 16:14:07 +0200

    14- Validating a Form Using Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,25 +9,18 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
 
     const errors = {};
-
-    const { account } = this.state;
-    if (this.state.account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (this.state.account.password.trim() === "")
-      errors.password = "Password is required.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (const item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
