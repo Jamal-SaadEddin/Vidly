cd803362be8bfdfc0a62c11b38b757255e19b263
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2023-09-21 08:57:55 +0300

    Some Fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Movies from "./components/movies";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Customers from "./components/customers";
